cb4b7c59090908525bff3f37c4c7c8cb9574cd61
arora <aroramadhav65@gmail.com>
2022-05-22 13:50:12 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:agora_uikit/agora_uikit.dart';
-import 'package:agora_uikit_example/agora_uikit.dart';
+import 'package:agora_uikit_example/app_id.dart
 import 'package:flutter/material.dart';
 
 // import './src/pages/index.dart';
