b08a2dd10309f38230c3f63296c95645a988b3ac
arora <aroramadhav65@gmail.com>
2022-05-22 13:43:46 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 import './src/pages/index.dart';
 
-void main() => runApp(
+void main() {
+  runApp(
       MyApp(),
     );
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
