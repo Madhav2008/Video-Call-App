315dec2539df5e9b79e5a7fcf39def8f11d89b28
arora <aroramadhav65@gmail.com>
2022-05-22 13:46:30 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   AgoraClient client = AgoraClient(
-    agoraConnectionData: AgoraConnectionData(appId: ),
+    agoraConnectionData: AgoraConnectionData(appId: appId),
     enabledPermission: [
       Permission.camera,
       Permission.microphone,
